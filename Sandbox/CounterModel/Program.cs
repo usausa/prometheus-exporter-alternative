5a38fddc01a35a87dc1c5787e8430ed48e2c1a3b
usausa <machi.pon@gmail.com>
2024-11-26 13:58:54 +0900

    Update

--- Sandbox/CounterModel/Program.cs
+++ Sandbox/CounterModel/Program.cs
@@ -1,38 +1,109 @@
 namespace CounterModel;
 
+using System;
 using System.Buffers;
-using System.Buffers.Text;
 using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 internal static class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
-        using var manager = new CounterManager();
+        using var manager = new MetricsManager();
+        using var plugin1 = new Plugin1(manager);
+        using var plugin2 = new Plugin2(manager);
 
+        while (true)
+        {
+            using var buffer = new PooledBufferWriter<byte>(65536);
+            await manager.CollectAsync(buffer, default!).ConfigureAwait(false);
+
+            var str = Encoding.UTF8.GetString(buffer.WrittenSpan);
+            Debug.WriteLine(str);
+
+            await Task.Delay(5000).ConfigureAwait(false);
+        }
+        // ReSharper disable once FunctionNeverReturns
     }
 }
 
 internal sealed class Plugin1 : IDisposable
 {
-    // TODO コールバックで値を設定、単体とタグあり、タグは変動
+    private readonly IGauge gauge2A;
+    private readonly IGauge gauge2B;
+
+    public Plugin1(MetricsManager manager)
+    {
+        var metrics = manager.CreateMetrics("plugin1_tagged");
+        gauge2A = metrics.CreateGauge(new("host", "server"), new("test", "2a"));
+        gauge2B = metrics.CreateGauge(new("host", "server"), new("test", "2b"));
+
+        manager.AddBeforeCollectCallback(Callback);
+    }
+
     public void Dispose()
     {
-        // TODO
+    }
+
+    private void Callback()
+    {
+        gauge2A.Value = DateTime.Now.AddDays(-1).Ticks;
+        gauge2B.Value = DateTime.Now.AddDays(1).Ticks;
     }
 }
 
-internal sealed class Plugin2
+internal sealed class Plugin2 : IDisposable
 {
-    // TODO タイマーで定期的に設定、単体とタグあり、タグは変動
+    private readonly IMetrics metrics;
+    private readonly List<IGauge> taggedGauges = [];
+
+    private readonly Timer timer;
+
+    private int counter;
+
+    public Plugin2(MetricsManager manager)
+    {
+        metrics = manager.CreateMetrics("plugin2_tagged");
+
+        timer = new Timer(Update, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(8000));
+    }
+
+    public void Dispose()
+    {
+        timer.Dispose();
+    }
+
+    private void Update(object? state)
+    {
+        counter++;
+
+        foreach (var gauge in taggedGauges)
+        {
+            gauge.Remove();
+        }
+        taggedGauges.Clear();
+
+        for (var i = 0; i < counter; i++)
+        {
+            var gauge = metrics.CreateGauge([new("name", $"no{i + 1}")]);
+            taggedGauges.Add(gauge);
+            gauge.Value = DateTime.Now.Second;
+        }
+
+        if (counter >= 3)
+        {
+            counter = 0;
+        }
+    }
 }
 
 // --------------------------------------------------------------------------------
 
-internal sealed class CounterManager : IDisposable
+internal sealed class MetricsManager : IDisposable
 {
-    private readonly List<IWriter> counters = [];
+    private readonly List<IMetrics> gauges = [];
 
     private readonly SemaphoreSlim semaphore = new(1);
 
@@ -44,40 +115,21 @@ internal sealed class CounterManager : IDisposable
         semaphore.Dispose();
     }
 
-    public ICounter<T> CreateCounter<T>(string name)
-        where T : struct
+    public IMetrics CreateMetrics(string name)
     {
-        var counter = new Counter<T>(name);
+        var gauge = new Metrics(name);
 
         semaphore.Wait(0);
         try
         {
-            counters.Add(counter);
+            gauges.Add(gauge);
         }
         finally
         {
             semaphore.Release();
         }
 
-        return counter;
-    }
-
-    public ITaggedCounter<T> CreateTaggedCounter<T>(string name)
-        where T : struct
-    {
-        var counter = new TaggedCounter<T>(name);
-
-        semaphore.Wait(0);
-        try
-        {
-            counters.Add(counter);
-        }
-        finally
-        {
-            semaphore.Release();
-        }
-
-        return counter;
+        return gauge;
     }
 
     public void AddBeforeCollectCallback(Action callback)
@@ -90,7 +142,7 @@ internal sealed class CounterManager : IDisposable
         beforeCollectAsyncCallbacks.Add(callback);
     }
 
-    public async Task Collect(IBufferWriter<byte> writer, CancellationToken cancel)
+    public async Task CollectAsync(IBufferWriter<byte> writer, CancellationToken cancel)
     {
         await semaphore.WaitAsync(0, cancel).ConfigureAwait(false);
         try
@@ -100,15 +152,14 @@ internal sealed class CounterManager : IDisposable
                 callback();
             }
 
-            await Task.WhenAll(beforeCollectAsyncCallbacks.Select((callback) => callback(cancel))).ConfigureAwait(false);
+            await Task.WhenAll(beforeCollectAsyncCallbacks.Select(callback => callback(cancel))).ConfigureAwait(false);
 
-            // TODO Write
-            foreach (var counter in counters)
+            foreach (var gauge in gauges)
             {
-                counter.Write(writer);
+                gauge.Write(writer);
             }
 
-            // TODO EOF
+            Helper.WriteEof(writer);
         }
         finally
         {
@@ -119,87 +170,59 @@ internal sealed class CounterManager : IDisposable
 
 // --------------------------------------------------------------------------------
 
-internal interface IWriter
+internal interface IMetrics
 {
+    IGauge CreateGauge(params KeyValuePair<string, object?>[] tags);
+
     void Write(IBufferWriter<byte> writer);
 }
 
-internal interface ICounter<T>
-    where T : struct
+internal interface IGauge
 {
-    T? Current { get; set; }
-}
+    double Value { get; set; }
 
-internal interface ITaggedCounter<T>
-    where T : struct
-{
-    ICounter<T> CreateCounter(params KeyValuePair<string, string>[] tags);
+    void Remove();
 }
 
 // --------------------------------------------------------------------------------
 
-internal sealed class Counter<T> : ICounter<T>, IWriter
-    where T : struct
+internal sealed class Metrics : IMetrics
 {
     private readonly string name;
 
     private readonly object sync = new();
 
-    public T? Current
-    {
-        get
-        {
-            lock (sync)
-            {
-                return field;
-            }
-        }
-        set
-        {
-            lock (sync)
-            {
-                field = value;
-            }
-        }
-    }
+    private readonly List<Gauge> entries = [];
 
-    public Counter(string name)
+    public Metrics(string name)
     {
         this.name = name;
     }
 
-    void IWriter.Write(IBufferWriter<byte> writer)
+    void IMetrics.Write(IBufferWriter<byte> writer)
     {
-        var value = Current;
-        if (value.HasValue)
+        Helper.WriteType(writer, name);
+
+        lock (sync)
         {
-            // TODO
-            //writer.
+            foreach (var entry in entries)
+            {
+                entry.Write(writer, name);
+            }
         }
     }
-}
 
-internal sealed class TaggedCounter<T> : ITaggedCounter<T>, IWriter
-    where T : struct
-{
-    private readonly string name;
-
-    private readonly object sync = new();
-
-    private readonly List<TaggedCounterEntry<T>> entries = [];
-
-    public TaggedCounter(string name)
+    public IGauge CreateGauge(params KeyValuePair<string, object?>[] tags)
     {
-        this.name = name;
+        lock (sync)
+        {
+            var gauge = new Gauge(this, tags);
+            entries.Add(gauge);
+            return gauge;
+        }
     }
 
-    // TODO
-    void IWriter.Write(IBufferWriter<byte> writer) => throw new NotImplementedException();
-
-    // TODO Create with tag dup check?
-    public ICounter<T> CreateCounter(params KeyValuePair<string, string>[] tags) => throw new NotImplementedException();
-
-    internal void Unregister(TaggedCounterEntry<T> entry)
+    internal void Unregister(Gauge entry)
     {
         lock (sync)
         {
@@ -208,62 +231,234 @@ internal sealed class TaggedCounter<T> : ITaggedCounter<T>, IWriter
     }
 }
 
-internal sealed class TaggedCounterEntry<T> : IDisposable
-    where T : struct
+internal sealed class Gauge : IGauge
 {
-    private readonly TaggedCounter<T> parent;
+    private readonly Metrics parent;
 
-    private readonly object sync = new();
+    private readonly Tag[] tags;
 
-    public T? Current
+    public double Value
     {
-        get
-        {
-            lock (sync)
-            {
-                return field;
-            }
-        }
-        set
-        {
-            lock (sync)
-            {
-                field = value;
-            }
-        }
+        get => Interlocked.Exchange(ref field, field);
+        set => Interlocked.Exchange(ref field, value);
     }
 
-    public TaggedCounterEntry(TaggedCounter<T> parent)
+    public Gauge(Metrics parent, KeyValuePair<string, object?>[] tags)
     {
         this.parent = parent;
+        this.tags = Helper.PrepareTags(tags);
     }
 
-    public void Dispose()
+    public void Remove()
     {
         parent.Unregister(this);
+    }
+
+    internal void Write(IBufferWriter<byte> writer, string name)
+    {
+        Helper.WriteValue(writer, name, Value, tags);
     }
 }
 
 // --------------------------------------------------------------------------------
 
-// TODO Operator
-internal static class ValueWriter
+internal record Tag(string Key, string Value);
+
+// --------------------------------------------------------------------------------
+
+internal static class Helper
 {
-    //internal interface IValueWriter<T>
-    //    where T : struct
-    //{
-    //    void Write(IBufferWriter<byte> writer, T value);
-    //}
+    private const byte Blank = (byte)' ';
+    private const byte LineFeed = (byte)'\n';
+    private const byte TagStart = (byte)'{';
+    private const byte TagEnd = (byte)'}';
+    private const byte Comma = (byte)',';
+    private const byte Equal = (byte)'=';
+    private const byte Quote = (byte)'"';
+    private const byte Solidus = (byte)'\\';
 
-    //internal static class Writer<T>
-    //{
-    //}
+    public static Tag[] PrepareTags(KeyValuePair<string, object?>[] tags)
+    {
+        var values = new Tag[tags.Length];
 
-    // TODO
-    // long, int
-    // ulong, uint
-    // double+f, float+f
-    //var buffer = new byte[128];
-    //Utf8Formatter.TryFormat(1.234d, buffer, out var written, StandardFormat.Parse("F2"));
-    //Debug.WriteLine(Encoding.UTF8.GetString(buffer.AsSpan(0, written)));
+        for (var i = 0; i < tags.Length; i++)
+        {
+            var tag = tags[i];
+            values[i] = new Tag(tag.Key, GetValueString(tag.Value));
+        }
+
+        return values;
+
+        static string GetValueString(object? value)
+        {
+            if (value is bool b)
+            {
+                return b ? "true" : "false";
+            }
+
+            return value?.ToString() ?? string.Empty;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void WriteByte(IBufferWriter<byte> writer, byte value)
+    {
+        var span = writer.GetSpan(1);
+        span[0] = value;
+        writer.Advance(1);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void WriteBytes(IBufferWriter<byte> writer, ReadOnlySpan<byte> value)
+    {
+        var span = writer.GetSpan(value.Length);
+        value.CopyTo(span);
+        writer.Advance(value.Length);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void WriteDouble(IBufferWriter<byte> writer, double value)
+    {
+        if (Double.IsFinite(value))
+        {
+            Span<char> buffer = stackalloc char[128];
+            value.TryFormat(buffer, out var written, "G", CultureInfo.InvariantCulture);
+
+            var span = writer.GetSpan(written);
+            for (var i = 0; i < written; i++)
+            {
+                span[i] = unchecked((byte)buffer[i]);
+            }
+
+            writer.Advance(written);
+        }
+        else if (Double.IsPositiveInfinity(value))
+        {
+            WriteBytes(writer, "+Inf"u8);
+        }
+        else if (Double.IsNegativeInfinity(value))
+        {
+            WriteBytes(writer, "-Inf"u8);
+        }
+        else
+        {
+            WriteBytes(writer, "Nan"u8);
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void WriteString(IBufferWriter<byte> writer, string value)
+    {
+        var span = writer.GetSpan(value.Length * 3);
+
+        var written = 0;
+        foreach (var c in value)
+        {
+            var ordinal = (ushort)c;
+            written += WriteUnicodeNoEscape(span[written..], ordinal);
+        }
+
+        writer.Advance(written);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void WriteEscapedString(IBufferWriter<byte> writer, string value)
+    {
+        var span = writer.GetSpan(value.Length * 3);
+
+        var written = 0;
+        foreach (var c in value)
+        {
+            var ordinal = (ushort)c;
+            switch (ordinal)
+            {
+                case Quote:
+                    span[written++] = Solidus;
+                    span[written++] = Quote;
+                    break;
+                case Solidus:
+                    span[written++] = Solidus;
+                    span[written++] = Solidus;
+                    break;
+                case LineFeed:
+                    span[written++] = Solidus;
+                    span[written++] = unchecked((byte)'n');
+                    break;
+                default:
+                    written += WriteUnicodeNoEscape(span[written..], ordinal);
+                    break;
+            }
+        }
+
+        writer.Advance(written);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int WriteUnicodeNoEscape(Span<byte> span, ushort ordinal)
+    {
+        var written = 0;
+        if (ordinal <= 0x7F)
+        {
+            span[written++] = unchecked((byte)ordinal);
+        }
+        else if (ordinal <= 0x07FF)
+        {
+            span[written++] = unchecked((byte)(0b_1100_0000 | (ordinal >> 6)));
+            span[written++] = unchecked((byte)(0b_1000_0000 | (ordinal & 0b_0011_1111)));
+        }
+        else
+        {
+            span[written++] = unchecked((byte)(0b_1110_0000 | (ordinal >> 12)));
+            span[written++] = unchecked((byte)(0b_1000_0000 | ((ordinal >> 6) & 0b_0011_1111)));
+            span[written++] = unchecked((byte)(0b_1000_0000 | (ordinal & 0b_0011_1111)));
+        }
+
+        return written;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteType(IBufferWriter<byte> writer, string name)
+    {
+        WriteBytes(writer, "# TYPE"u8);
+        WriteByte(writer, Blank);
+        WriteString(writer, name);
+        WriteByte(writer, Blank);
+        WriteBytes(writer, "gauge"u8);
+        WriteByte(writer, LineFeed);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteEof(IBufferWriter<byte> writer)
+    {
+        WriteBytes(writer, "# EOF"u8);
+        WriteByte(writer, LineFeed);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteValue(IBufferWriter<byte> writer, string name, double value, Tag[] tags)
+    {
+        WriteString(writer, name);
+        if (tags.Length > 0)
+        {
+            WriteByte(writer, TagStart);
+            for (var i = 0; i < tags.Length; i++)
+            {
+                if (i > 0)
+                {
+                    WriteByte(writer, Comma);
+                }
+
+                var tag = tags[i];
+                WriteString(writer, tag.Key);
+                WriteByte(writer, Equal);
+                WriteByte(writer, Quote);
+                WriteEscapedString(writer, tag.Value);
+                WriteByte(writer, Quote);
+            }
+            WriteByte(writer, TagEnd);
+        }
+        WriteByte(writer, Blank);
+        WriteDouble(writer, value);
+        WriteByte(writer, LineFeed);
+    }
 }

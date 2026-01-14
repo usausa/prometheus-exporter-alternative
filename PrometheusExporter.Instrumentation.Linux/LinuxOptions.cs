11d0219319e170de765b6b45d0654d9e5e1313f8
usausa <machi.pon@gmail.com>
2026-01-14 13:08:45 +0900

    Update

--- PrometheusExporter.Instrumentation.Linux/LinuxOptions.cs
+++ PrometheusExporter.Instrumentation.Linux/LinuxOptions.cs
@@ -3,4 +3,36 @@ namespace PrometheusExporter.Instrumentation.Linux;
 internal sealed class LinuxOptions
 {
     public int UpdateDuration { get; set; } = 1000;
+
+    public bool Uptime { get; set; } = true;
+
+    public bool Statics { get; set; } = true;
+
+    public bool LoadAverage { get; set; } = true;
+
+    public bool Memory { get; set; } = true;
+
+    public bool VirtualMemory { get; set; } = true;
+
+    public bool Partition { get; set; } = true;
+
+    public bool DiskStatics { get; set; } = true;
+
+    public bool FileDescriptor { get; set; } = true;
+
+    public bool NetworkStatic { get; set; } = true;
+
+    public bool TcpStatic { get; set; } = true;
+
+    public bool Tcp6Static { get; set; } = true;
+
+    public bool ProcessSummary { get; set; } = true;
+
+    public bool Cpu { get; set; } = true;
+
+    public bool Battery { get; set; } = true;
+
+    public bool MainsAdapter { get; set; } = true;
+
+    public bool HardwareMonitor { get; set; } = true;
 }

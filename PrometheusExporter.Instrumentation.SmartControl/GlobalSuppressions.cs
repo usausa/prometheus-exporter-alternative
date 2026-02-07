0cb58ca52a5a029160671195a3da30690d2be397
usausa <machi.pon@gmail.com>
2026-02-07 14:03:03 +0900

    Update

--- PrometheusExporter.Instrumentation.SmartControl/GlobalSuppressions.cs
+++
@@ -1,1 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Ignore")]
--- PrometheusExporter.Instrumentation.SmartControl/SmartControlOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace PrometheusExporter.Instrumentation.SmartControl;
-
-internal sealed class SmartControlOptions
-{
-    public int Interval { get; set; } = 600_000;
-}

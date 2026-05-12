e8473ba58976583d01ca4a5397f9534d3d5d6641
usausa <machi.pon@gmail.com>
2026-05-12 16:15:15 +0900

    Update setting

--- PrometheusExporter.Instrumentation.Mac/MacInstrumentation.cs
+++ PrometheusExporter.Instrumentation.Mac/MacInstrumentation.cs
@@ -24,7 +24,62 @@ internal sealed class MacInstrumentation
         host = environment.Host;
         updateDuration = TimeSpan.FromMilliseconds(options.UpdateDuration);
 
-        SetupUptimeMetric(manager);
+        if (options.Uptime)
+        {
+            SetupUptimeMetric(manager);
+        }
+        if (options.Cpu)
+        {
+            SetupCpuMetric(manager);
+        }
+        if (options.LoadAverage)
+        {
+            SetupLoadAverageMetric(manager);
+        }
+        if (options.Memory)
+        {
+            SetupMemoryMetric(manager);
+        }
+        if (options.SwapUsage)
+        {
+            SetupSwapUsageMetric(manager);
+        }
+        if (options.FileSystem)
+        {
+            SetupFileSystemMetric(manager);
+        }
+        if (options.Disk)
+        {
+            SetupDiskMetric(manager);
+        }
+        if (options.FileDescriptor)
+        {
+            SetupFileDescriptorMetric(manager);
+        }
+        if (options.Network)
+        {
+            SetupNetworkMetric(manager);
+        }
+        if (options.ProcessSummary)
+        {
+            SetupProcessSummaryMetric(manager);
+        }
+        if (options.CpuFrequency)
+        {
+            SetupCpuFrequencyMetric(manager);
+        }
+        if (options.Gpu)
+        {
+            SetupGpuMetric(manager);
+        }
+        if (options.Power)
+        {
+            SetupPowerMetric(manager);
+        }
+        if (options.HardwareMonitor)
+        {
+            SetupHardwareMonitorMetric(manager);
+        }
 
         manager.AddBeforeCollectCallback(Update);
     }
@@ -58,7 +113,20 @@ internal sealed class MacInstrumentation
     // Helper
     //--------------------------------------------------------------------------------
 
-    private KeyValuePair<string, object?>[] MakeTags() => [new("host", host)];
+    private KeyValuePair<string, object?>[] MakeTags(params KeyValuePair<string, object?>[] options)
+    {
+        if (options.Length == 0)
+        {
+            return [new("host", host)];
+        }
+
+        var tags = new List<KeyValuePair<string, object?>>([new("host", host)]);
+        tags.AddRange(options);
+        return [.. tags];
+    }
+
+    //private static bool IsTarget(IEnumerable<string> targets, string name) =>
+    //    targets.Any(x => (x == "*") || (x == name));
 
     private static Action MakeEntry(Func<double> measurement, IMetricSeries series)
     {
@@ -78,5 +146,129 @@ internal sealed class MacInstrumentation
 
         var metric = manager.CreateGauge("system_uptime");
         updateEntries.Add(MakeEntry(() => uptimeInfo.Elapsed.TotalSeconds, metric.Create(MakeTags())));
+    }
+
+    //--------------------------------------------------------------------------------
+    // Cpu
+    //--------------------------------------------------------------------------------
+
+    private void SetupCpuMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // LoadAverage
+    //--------------------------------------------------------------------------------
+
+    private void SetupLoadAverageMetric(IMetricManager manager)
+    {
+        var load = PlatformProvider.GetLoadAverage();
+
+        prepareEntries.Add(() => load.Update());
+
+        var metric = manager.CreateGauge("system_load_average");
+        updateEntries.Add(MakeEntry(() => load.Average1, metric.Create(MakeTags([new("window", 1)]))));
+        updateEntries.Add(MakeEntry(() => load.Average5, metric.Create(MakeTags([new("window", 5)]))));
+        updateEntries.Add(MakeEntry(() => load.Average15, metric.Create(MakeTags([new("window", 15)]))));
+    }
+
+    //--------------------------------------------------------------------------------
+    // Memory
+    //--------------------------------------------------------------------------------
+
+    private void SetupMemoryMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // SwapUsage
+    //--------------------------------------------------------------------------------
+
+    private void SetupSwapUsageMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // FileSystem
+    //--------------------------------------------------------------------------------
+
+    private void SetupFileSystemMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // Disk
+    //--------------------------------------------------------------------------------
+
+    private void SetupDiskMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // FileDescriptor
+    //--------------------------------------------------------------------------------
+
+    private void SetupFileDescriptorMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // Network
+    //--------------------------------------------------------------------------------
+
+    private void SetupNetworkMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // ProcessSummary
+    //--------------------------------------------------------------------------------
+
+    private void SetupProcessSummaryMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // CpuFrequency
+    //--------------------------------------------------------------------------------
+
+    private void SetupCpuFrequencyMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // Gpu
+    //--------------------------------------------------------------------------------
+
+    private void SetupGpuMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // Power
+    //--------------------------------------------------------------------------------
+
+    private void SetupPowerMetric(IMetricManager manager)
+    {
+        // TODO
+    }
+
+    //--------------------------------------------------------------------------------
+    // HardwareMonitor
+    //--------------------------------------------------------------------------------
+
+    private void SetupHardwareMonitorMetric(IMetricManager manager)
+    {
+        // TODO
     }
 }
--- PrometheusExporter.Instrumentation.Mac/MacOptions.cs
+++ PrometheusExporter.Instrumentation.Mac/MacOptions.cs
@@ -3,4 +3,32 @@ namespace PrometheusExporter.Instrumentation.Mac;
 internal sealed class MacOptions
 {
     public int UpdateDuration { get; set; } = 1000;
+
+    public bool Uptime { get; set; } = true;
+
+    public bool Cpu { get; set; } = true;
+
+    public bool LoadAverage { get; set; } = true;
+
+    public bool Memory { get; set; } = true;
+
+    public bool SwapUsage { get; set; } = true;
+
+    public bool FileSystem { get; set; } = true;
+
+    public bool Disk { get; set; } = true;
+
+    public bool FileDescriptor { get; set; } = true;
+
+    public bool Network { get; set; } = true;
+
+    public bool ProcessSummary { get; set; } = true;
+
+    public bool CpuFrequency { get; set; } = true;
+
+    public bool Gpu { get; set; } = true;
+
+    public bool Power { get; set; } = true;
+
+    public bool HardwareMonitor { get; set; } = true;
 }

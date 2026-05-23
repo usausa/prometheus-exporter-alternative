f76b330927f6458986c348220c7482865f787861
usausa <machi.pon@gmail.com>
2026-05-23 16:48:13 +0900

    Update

---
+++ PrometheusExporter.Instrumentation.DiskInfo.Mac/DiskInfoInstrumentation.cs
@@ -0,0 +1,228 @@
+namespace PrometheusExporter.Instrumentation.DiskInfo;
+
+using MacDotNet.Disk;
+
+using PrometheusExporter.Abstractions;
+
+internal sealed class DiskInfoInstrumentation : IDisposable
+{
+    private readonly TimeSpan updateDuration;
+
+    private readonly IReadOnlyList<IDiskInfo> disks;
+
+    private readonly List<NvmeDisk> nvmeDisks = [];
+
+    private readonly List<GenericDisk> genericDisks = [];
+
+    private DateTime lastUpdate;
+
+    public DiskInfoInstrumentation(
+        DiskInfoOptions options,
+        IInstrumentationEnvironment environment,
+        IMetricManager manager)
+    {
+        updateDuration = TimeSpan.FromMilliseconds(options.UpdateDuration);
+
+        var sectorMetric = manager.CreateGauge("smart_disk_byte_per_sector");
+        var nvmeMetric = manager.CreateGauge("smart_nvme_value");
+        var genericMetric = manager.CreateGauge("smart_generic_value");
+
+        disks = DiskInfo.GetInformation();
+
+        foreach (var disk in disks)
+        {
+            var device = Path.GetFileName(disk.DeviceName ?? string.Empty);
+            var sector = sectorMetric.Create(MakeTags(environment.Host, disk.Index, disk.Model, device));
+            sector.Value = disk.LogicalBlockSize;
+
+            if (disk.SmartType == SmartType.Nvme)
+            {
+                var smart = (ISmartNvme)disk.Smart;
+                nvmeDisks.Add(new NvmeDisk(MakeNvmeMetrics(nvmeMetric, smart, environment.Host, disk, device), smart));
+            }
+            else if (disk.SmartType == SmartType.Generic)
+            {
+                var smart = (ISmartGeneric)disk.Smart;
+                genericDisks.Add(new GenericDisk(MakeGenericMetrics(genericMetric, smart, environment.Host, disk, device), smart));
+            }
+        }
+
+        manager.AddBeforeCollectCallback(Update);
+    }
+
+    public void Dispose()
+    {
+        foreach (var disk in disks)
+        {
+            disk.Dispose();
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    // Event
+    //--------------------------------------------------------------------------------
+
+    private void Update()
+    {
+        var now = DateTime.Now;
+        if ((now - lastUpdate) < updateDuration)
+        {
+            return;
+        }
+
+        foreach (var disk in nvmeDisks)
+        {
+            disk.Update();
+        }
+
+        foreach (var disk in genericDisks)
+        {
+            disk.Update();
+        }
+
+        lastUpdate = now;
+    }
+
+    //--------------------------------------------------------------------------------
+    // Helper
+    //--------------------------------------------------------------------------------
+
+    private static KeyValuePair<string, object?>[] MakeTags(string host, uint index, string model, string device) =>
+        [new("host", host), new("index", index), new("model", model), new("device", device)];
+
+    private static KeyValuePair<string, object?>[] MakeTags(string host, uint index, string model, string device, string id) =>
+        [new("host", host), new("index", index), new("model", model), new("device", device), new("smart_id", id)];
+
+    private static IMetricSeries[] MakeNvmeMetrics(IMetric metric, ISmartNvme smart, string host, IDiskInfo disk, string device)
+    {
+        IMetricSeries Factory(string id) => metric.Create(MakeTags(host, disk.Index, disk.Model, device, id));
+
+        var entries = new IMetricSeries[17 + smart.TemperatureSensors.Length];
+        entries[0] = Factory("available_spare");
+        entries[1] = Factory("available_spare_threshold");
+        entries[2] = Factory("controller_busy_time");
+        entries[3] = Factory("critical_composite_temperature_time");
+        entries[4] = Factory("critical_warning");
+        entries[5] = Factory("data_unit_read");
+        entries[6] = Factory("data_unit_written");
+        entries[7] = Factory("error_info_log_entries");
+        entries[8] = Factory("host_read_commands");
+        entries[9] = Factory("host_write_commands");
+        entries[10] = Factory("media_errors");
+        entries[11] = Factory("percentage_used");
+        entries[12] = Factory("power_cycles");
+        entries[13] = Factory("power_on_hours");
+        entries[14] = Factory("temperature");
+        entries[15] = Factory("unsafe_shutdowns");
+        entries[16] = Factory("warning_composite_temperature_time");
+        for (var i = 0; i < smart.TemperatureSensors.Length; i++)
+        {
+            entries[17 + i] = Factory($"temperature_sensor{i}");
+        }
+
+        return entries;
+    }
+
+    private static IMetricSeries?[] MakeGenericMetrics(IMetric metric, ISmartGeneric smart, string host, IDiskInfo disk, string device)
+    {
+        IMetricSeries Factory(string id) => metric.Create(MakeTags(host, disk.Index, disk.Model, device, id));
+
+        var entries = new IMetricSeries?[256];
+        foreach (var smartId in smart.GetSupportedIds())
+        {
+            var id = (byte)smartId;
+            entries[id] = Factory($"{id:X2}");
+        }
+
+        return entries;
+    }
+
+    //--------------------------------------------------------------------------------
+    // Disk
+    //--------------------------------------------------------------------------------
+
+    private sealed class NvmeDisk
+    {
+        private readonly IMetricSeries[] entries;
+
+        private readonly ISmartNvme smart;
+
+        public NvmeDisk(IMetricSeries[] entries, ISmartNvme smart)
+        {
+            this.entries = entries;
+            this.smart = smart;
+        }
+
+        public void Update()
+        {
+            if (smart.Update())
+            {
+                entries[0].Value = smart.AvailableSpare;
+                entries[1].Value = smart.AvailableSpareThreshold;
+                entries[2].Value = smart.ControllerBusyTime;
+                entries[3].Value = smart.CriticalCompositeTemperatureTime;
+                entries[4].Value = smart.CriticalWarning;
+                entries[5].Value = smart.DataUnitRead;
+                entries[6].Value = smart.DataUnitWritten;
+                entries[7].Value = smart.ErrorInfoLogEntries;
+                entries[8].Value = smart.HostReadCommands;
+                entries[9].Value = smart.HostWriteCommands;
+                entries[10].Value = smart.MediaErrors;
+                entries[11].Value = smart.PercentageUsed;
+                entries[12].Value = smart.PowerCycles;
+                entries[13].Value = smart.PowerOnHours;
+                entries[14].Value = smart.Temperature;
+                entries[15].Value = smart.UnsafeShutdowns;
+                entries[16].Value = smart.WarningCompositeTemperatureTime;
+                for (var i = 0; i < smart.TemperatureSensors.Length; i++)
+                {
+                    var value = smart.TemperatureSensors[i];
+                    entries[17 + i].Value = value >= 0 ? value : double.NaN;
+                }
+            }
+            else
+            {
+                foreach (var gauge in entries)
+                {
+                    gauge.Value = double.NaN;
+                }
+            }
+        }
+    }
+
+    private sealed class GenericDisk
+    {
+        private readonly IMetricSeries?[] entries;
+
+        private readonly ISmartGeneric smart;
+
+        public GenericDisk(IMetricSeries?[] entries, ISmartGeneric smart)
+        {
+            this.entries = entries;
+            this.smart = smart;
+        }
+
+        public void Update()
+        {
+            if (smart.Update())
+            {
+                foreach (var id in smart.GetSupportedIds())
+                {
+                    var gauge = entries[(byte)id];
+                    if (gauge is not null)
+                    {
+                        var attr = smart.GetAttribute(id);
+                        gauge.Value = attr?.RawValue ?? double.NaN;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var gauge in entries)
+                {
+                    gauge?.Value = double.NaN;
+                }
+            }
+        }
+    }
+}
--- PrometheusExporter.Instrumentation.Mac/MacInstrumentation.cs
+++ PrometheusExporter.Instrumentation.Mac/MacInstrumentation.cs
@@ -154,7 +154,75 @@ internal sealed class MacInstrumentation
 
     private void SetupCpuMetric(IMetricManager manager)
     {
-        // TODO
+        var cpu = PlatformProvider.GetCpuStat();
+
+        var corePrevious = Array.Empty<PreviousCpuTotal>();
+
+        prepareEntries.Add(() =>
+        {
+            if (corePrevious.Length < cpu.CpuCores.Count)
+            {
+                var newPrevious = new PreviousCpuTotal[cpu.CpuCores.Count];
+                for (var i = 0; i < newPrevious.Length; i++)
+                {
+                    newPrevious[i] = corePrevious.Length > i ? corePrevious[i] : new PreviousCpuTotal();
+                }
+                corePrevious = newPrevious;
+            }
+
+            for (var i = 0; i < corePrevious.Length && i < cpu.CpuCores.Count; i++)
+            {
+                UpdatePrevious(corePrevious[i], cpu.CpuCores[i]);
+            }
+
+            cpu.Update();
+        });
+
+        var metricCpuTime = manager.CreateCounter("system_cpu_time_total");
+        var metricCpuLoad = manager.CreateGauge("system_cpu_load");
+
+        for (var i = 0; i < cpu.CpuCores.Count; i++)
+        {
+            var core = cpu.CpuCores[i];
+            updateEntries.Add(MakeEntry(() => core.User, metricCpuTime.Create(MakeTags(new("name", $"cpu{core.Number}"), new("mode", "user")))));
+            updateEntries.Add(MakeEntry(() => core.System, metricCpuTime.Create(MakeTags(new("name", $"cpu{core.Number}"), new("mode", "system")))));
+            updateEntries.Add(MakeEntry(() => core.Idle, metricCpuTime.Create(MakeTags(new("name", $"cpu{core.Number}"), new("mode", "idle")))));
+            updateEntries.Add(MakeEntry(() => core.Nice, metricCpuTime.Create(MakeTags(new("name", $"cpu{core.Number}"), new("mode", "nice")))));
+
+            var coreCapture = core;
+            var prevIndex = i;
+            updateEntries.Add(MakeEntry(() =>
+            {
+                if (prevIndex >= corePrevious.Length)
+                {
+                    return 0;
+                }
+
+                var prev = corePrevious[prevIndex];
+                var nonIdle = (double)(coreCapture.User + coreCapture.System + coreCapture.Nice);
+                var total = nonIdle + coreCapture.Idle;
+                var totalDiff = total - prev.Total;
+                var nonIdleDiff = nonIdle - prev.NonIdle;
+                return totalDiff == 0 ? 0 : nonIdleDiff / totalDiff * 100.0;
+            }, metricCpuLoad.Create(MakeTags([new("name", $"cpu{core.Number}")]))));
+        }
+
+        return;
+
+        static void UpdatePrevious(PreviousCpuTotal previous, CpuCoreStat core)
+        {
+            var nonIdle = (double)(core.User + core.System + core.Nice);
+            var idle = (double)core.Idle;
+            previous.NonIdle = nonIdle;
+            previous.Total = idle + nonIdle;
+        }
+    }
+
+    private sealed class PreviousCpuTotal
+    {
+        public double NonIdle { get; set; }
+
+        public double Total { get; set; }
     }
 
     //--------------------------------------------------------------------------------
@@ -179,7 +247,30 @@ internal sealed class MacInstrumentation
 
     private void SetupMemoryMetric(IMetricManager manager)
     {
-        // TODO
+        var memory = PlatformProvider.GetMemoryStat();
+
+        prepareEntries.Add(() => memory.Update());
+
+        var metricMem = manager.CreateGauge("system_memory_mem");
+        updateEntries.Add(MakeEntry(() => memory.PhysicalMemory, metricMem.Create(MakeTags([new("type", "total")]))));
+        updateEntries.Add(MakeEntry(() => (memory.ActiveCount + memory.WireCount) * memory.PageSize, metricMem.Create(MakeTags([new("type", "used")]))));
+        updateEntries.Add(MakeEntry(() => memory.FreeCount * memory.PageSize, metricMem.Create(MakeTags([new("type", "free")]))));
+        updateEntries.Add(MakeEntry(() => memory.InactiveCount * memory.PageSize, metricMem.Create(MakeTags([new("type", "inactive")]))));
+
+        var metricLoad = manager.CreateGauge("system_memory_load");
+        updateEntries.Add(MakeEntry(() =>
+        {
+            var used = memory.AppMemoryBytes + memory.WiredBytes;
+            return memory.PhysicalMemory > 0 ? (double)used / memory.PhysicalMemory * 100.0 : 0;
+        }, metricLoad.Create(MakeTags())));
+
+        var metricPage = manager.CreateCounter("system_memory_page_total");
+        updateEntries.Add(MakeEntry(() => memory.PageIn, metricPage.Create(MakeTags([new("type", "in")]))));
+        updateEntries.Add(MakeEntry(() => memory.PageOut, metricPage.Create(MakeTags([new("type", "out")]))));
+
+        var metricSwapIO = manager.CreateCounter("system_memory_swap_io_total");
+        updateEntries.Add(MakeEntry(() => memory.SwapIn, metricSwapIO.Create(MakeTags([new("type", "in")]))));
+        updateEntries.Add(MakeEntry(() => memory.SwapOut, metricSwapIO.Create(MakeTags([new("type", "out")]))));
     }
 
     //--------------------------------------------------------------------------------
@@ -188,7 +279,14 @@ internal sealed class MacInstrumentation
 
     private void SetupSwapUsageMetric(IMetricManager manager)
     {
-        // TODO
+        var swap = PlatformProvider.GetSwapUsage();
+
+        prepareEntries.Add(() => swap.Update());
+
+        var metric = manager.CreateGauge("system_swap");
+        updateEntries.Add(MakeEntry(() => swap.TotalBytes, metric.Create(MakeTags([new("type", "total")]))));
+        updateEntries.Add(MakeEntry(() => swap.UsedBytes, metric.Create(MakeTags([new("type", "used")]))));
+        updateEntries.Add(MakeEntry(() => swap.AvailableBytes, metric.Create(MakeTags([new("type", "available")]))));
     }
 
     //--------------------------------------------------------------------------------
@@ -197,7 +295,32 @@ internal sealed class MacInstrumentation
 
     private void SetupFileSystemMetric(IMetricManager manager)
     {
-        // TODO
+        var fs = PlatformProvider.GetFileSystemStat();
+
+        prepareEntries.Add(() => fs.Update());
+
+        var metricSizeUsed = manager.CreateGauge("system_partition_size_used");
+        var metricSizeTotal = manager.CreateGauge("system_partition_size_total");
+        var metricSizeFree = manager.CreateGauge("system_partition_size_free");
+        var metricSizeAvailable = manager.CreateGauge("system_partition_size_available");
+        var metricFilesTotal = manager.CreateGauge("system_partition_files_total");
+        var metricFilesFree = manager.CreateGauge("system_partition_files_free");
+
+        updateEntries.Add(() =>
+        {
+            foreach (var entry in fs.Entries)
+            {
+                var tags = MakeTags(new("name", entry.DeviceName), new("mount", entry.MountPoint), new("fs", entry.FileSystem));
+                var used = entry.TotalSize - entry.FreeSize;
+                var total = used + entry.AvailableSize;
+                metricSizeUsed.Create(tags).Value = total > 0 ? (double)used / total * 100.0 : 0;
+                metricSizeTotal.Create(tags).Value = entry.TotalSize;
+                metricSizeFree.Create(tags).Value = entry.FreeSize;
+                metricSizeAvailable.Create(tags).Value = entry.AvailableSize;
+                metricFilesTotal.Create(tags).Value = entry.TotalFiles;
+                metricFilesFree.Create(tags).Value = entry.FreeFiles;
+            }
+        });
     }
 
     //--------------------------------------------------------------------------------
@@ -206,7 +329,29 @@ internal sealed class MacInstrumentation
 
     private void SetupDiskMetric(IMetricManager manager)
     {
-        // TODO
+        var disk = PlatformProvider.GetDiskStat();
+
+        prepareEntries.Add(() => disk.Update());
+
+        var metricBytesRead = manager.CreateCounter("system_disk_bytes_total");
+        var metricCompleted = manager.CreateCounter("system_disk_completed_total");
+        var metricTime = manager.CreateCounter("system_disk_time_total");
+        var metricErrors = manager.CreateCounter("system_disk_errors_total");
+
+        updateEntries.Add(() =>
+        {
+            foreach (var device in disk.Devices)
+            {
+                metricBytesRead.Create(MakeTags(new("name", device.BsdName), new("type", "read"))).Value = device.BytesRead;
+                metricBytesRead.Create(MakeTags(new("name", device.BsdName), new("type", "write"))).Value = device.BytesWrite;
+                metricCompleted.Create(MakeTags(new("name", device.BsdName), new("type", "read"))).Value = device.ReadsCompleted;
+                metricCompleted.Create(MakeTags(new("name", device.BsdName), new("type", "write"))).Value = device.WritesCompleted;
+                metricTime.Create(MakeTags(new("name", device.BsdName), new("type", "read"))).Value = device.TotalTimeRead;
+                metricTime.Create(MakeTags(new("name", device.BsdName), new("type", "write"))).Value = device.TotalTimeWrite;
+                metricErrors.Create(MakeTags(new("name", device.BsdName), new("type", "read"))).Value = device.ErrorsRead;
+                metricErrors.Create(MakeTags(new("name", device.BsdName), new("type", "write"))).Value = device.ErrorsWrite;
+            }
+        });
     }
 
     //--------------------------------------------------------------------------------
@@ -215,7 +360,15 @@ internal sealed class MacInstrumentation
 
     private void SetupFileDescriptorMetric(IMetricManager manager)
     {
-        // TODO
+        var fd = PlatformProvider.GetFileHandleStat();
+
+        prepareEntries.Add(() => fd.Update());
+
+        var metricOpenFiles = manager.CreateGauge("system_fd_open_files");
+        updateEntries.Add(MakeEntry(() => fd.OpenFiles, metricOpenFiles.Create(MakeTags())));
+
+        var metricOpenVnodes = manager.CreateGauge("system_fd_open_vnodes");
+        updateEntries.Add(MakeEntry(() => fd.OpenVnodes, metricOpenVnodes.Create(MakeTags())));
     }
 
     //--------------------------------------------------------------------------------
@@ -224,7 +377,33 @@ internal sealed class MacInstrumentation
 
     private void SetupNetworkMetric(IMetricManager manager)
     {
-        // TODO
+        var network = PlatformProvider.GetNetworkStat();
+
+        prepareEntries.Add(() => network.Update());
+
+        var metricBytes = manager.CreateCounter("system_network_bytes_total");
+        var metricPackets = manager.CreateCounter("system_network_packets_total");
+        var metricErrors = manager.CreateCounter("system_network_errors_total");
+        var metricDropped = manager.CreateCounter("system_network_dropped_total");
+        var metricMulticast = manager.CreateCounter("system_network_multicast_total");
+        var metricCollisions = manager.CreateCounter("system_network_collisions_total");
+
+        updateEntries.Add(() =>
+        {
+            foreach (var nif in network.Interfaces)
+            {
+                metricBytes.Create(MakeTags(new("name", nif.Name), new("type", "rx"))).Value = nif.RxBytes;
+                metricBytes.Create(MakeTags(new("name", nif.Name), new("type", "tx"))).Value = nif.TxBytes;
+                metricPackets.Create(MakeTags(new("name", nif.Name), new("type", "rx"))).Value = nif.RxPackets;
+                metricPackets.Create(MakeTags(new("name", nif.Name), new("type", "tx"))).Value = nif.TxPackets;
+                metricErrors.Create(MakeTags(new("name", nif.Name), new("type", "rx"))).Value = nif.RxErrors;
+                metricErrors.Create(MakeTags(new("name", nif.Name), new("type", "tx"))).Value = nif.TxErrors;
+                metricDropped.Create(MakeTags(new("name", nif.Name), new("type", "rx"))).Value = nif.RxDrops;
+                metricMulticast.Create(MakeTags(new("name", nif.Name), new("type", "rx"))).Value = nif.RxMulticast;
+                metricMulticast.Create(MakeTags(new("name", nif.Name), new("type", "tx"))).Value = nif.TxMulticast;
+                metricCollisions.Create(MakeTags(new("name", nif.Name), new("type", "tx"))).Value = nif.Collisions;
+            }
+        });
     }
 
     //--------------------------------------------------------------------------------
@@ -233,7 +412,15 @@ internal sealed class MacInstrumentation
 
     private void SetupProcessSummaryMetric(IMetricManager manager)
     {
-        // TODO
+        var process = PlatformProvider.GetProcessSummary();
+
+        prepareEntries.Add(() => process.Update());
+
+        var metricProcess = manager.CreateGauge("system_process_count");
+        updateEntries.Add(MakeEntry(() => process.ProcessCount, metricProcess.Create(MakeTags())));
+
+        var metricThread = manager.CreateGauge("system_thread_count");
+        updateEntries.Add(MakeEntry(() => process.ThreadCount, metricThread.Create(MakeTags())));
     }
 
     //--------------------------------------------------------------------------------
@@ -242,7 +429,20 @@ internal sealed class MacInstrumentation
 
     private void SetupCpuFrequencyMetric(IMetricManager manager)
     {
-        // TODO
+        var cpuFreq = PlatformProvider.GetCpuFrequency();
+
+        prepareEntries.Add(() => cpuFreq.Update());
+
+        var metric = manager.CreateGauge("hardware_cpu_frequency");
+
+        updateEntries.Add(() =>
+        {
+            foreach (var core in cpuFreq.Cores)
+            {
+                var coreType = core.CoreType == CpuCoreType.Efficiency ? "efficiency" : "performance";
+                metric.Create(MakeTags(new("name", $"cpu{core.Number}"), new("type", coreType))).Value = core.Frequency;
+            }
+        });
     }
 
     //--------------------------------------------------------------------------------
@@ -251,7 +451,27 @@ internal sealed class MacInstrumentation
 
     private void SetupGpuMetric(IMetricManager manager)
     {
-        // TODO
+        var gpus = PlatformProvider.GetGpuDevices();
+
+        prepareEntries.Add(() =>
+        {
+            foreach (var gpu in gpus)
+            {
+                gpu.Update();
+            }
+        });
+
+        var metricUtilization = manager.CreateGauge("hardware_gpu_utilization");
+        var metricMemory = manager.CreateGauge("hardware_gpu_memory");
+
+        foreach (var gpu in gpus)
+        {
+            updateEntries.Add(MakeEntry(() => gpu.DeviceUtilization, metricUtilization.Create(MakeTags(new("name", gpu.Name), new("type", "device")))));
+            updateEntries.Add(MakeEntry(() => gpu.RendererUtilization, metricUtilization.Create(MakeTags(new("name", gpu.Name), new("type", "renderer")))));
+            updateEntries.Add(MakeEntry(() => gpu.TilerUtilization, metricUtilization.Create(MakeTags(new("name", gpu.Name), new("type", "tiler")))));
+            updateEntries.Add(MakeEntry(() => gpu.AllocSystemMemory, metricMemory.Create(MakeTags(new("name", gpu.Name), new("type", "alloc")))));
+            updateEntries.Add(MakeEntry(() => gpu.InUseSystemMemory, metricMemory.Create(MakeTags(new("name", gpu.Name), new("type", "in_use")))));
+        }
     }
 
     //--------------------------------------------------------------------------------
@@ -260,7 +480,21 @@ internal sealed class MacInstrumentation
 
     private void SetupPowerMetric(IMetricManager manager)
     {
-        // TODO
+        var power = PlatformProvider.GetPowerStat();
+        if (!power.Supported)
+        {
+            return;
+        }
+
+        prepareEntries.Add(() => power.Update());
+
+        var metric = manager.CreateGauge("hardware_power_energy");
+        updateEntries.Add(MakeEntry(() => power.Cpu, metric.Create(MakeTags([new("type", "cpu")]))));
+        updateEntries.Add(MakeEntry(() => power.Gpu, metric.Create(MakeTags([new("type", "gpu")]))));
+        updateEntries.Add(MakeEntry(() => power.Ane, metric.Create(MakeTags([new("type", "ane")]))));
+        updateEntries.Add(MakeEntry(() => power.Ram, metric.Create(MakeTags([new("type", "ram")]))));
+        updateEntries.Add(MakeEntry(() => power.Pci, metric.Create(MakeTags([new("type", "pci")]))));
+        updateEntries.Add(MakeEntry(() => power.Total, metric.Create(MakeTags([new("type", "total")]))));
     }
 
     //--------------------------------------------------------------------------------
@@ -269,6 +503,53 @@ internal sealed class MacInstrumentation
 
     private void SetupHardwareMonitorMetric(IMetricManager manager)
     {
-        // TODO
+        var smc = PlatformProvider.GetSmcMonitor();
+
+        prepareEntries.Add(() => smc.Update());
+
+        if (smc.Temperatures.Count > 0)
+        {
+            var metric = manager.CreateGauge("hardware_monitor_temperature");
+            foreach (var sensor in smc.Temperatures)
+            {
+                updateEntries.Add(MakeEntry(() => sensor.Value, metric.Create(MakeTags(new("key", sensor.Key), new("description", sensor.Description)))));
+            }
+        }
+
+        if (smc.Voltages.Count > 0)
+        {
+            var metric = manager.CreateGauge("hardware_monitor_voltage");
+            foreach (var sensor in smc.Voltages)
+            {
+                updateEntries.Add(MakeEntry(() => sensor.Value, metric.Create(MakeTags(new("key", sensor.Key), new("description", sensor.Description)))));
+            }
+        }
+
+        if (smc.Currents.Count > 0)
+        {
+            var metric = manager.CreateGauge("hardware_monitor_current");
+            foreach (var sensor in smc.Currents)
+            {
+                updateEntries.Add(MakeEntry(() => sensor.Value, metric.Create(MakeTags(new("key", sensor.Key), new("description", sensor.Description)))));
+            }
+        }
+
+        if (smc.Powers.Count > 0)
+        {
+            var metric = manager.CreateGauge("hardware_monitor_power");
+            foreach (var sensor in smc.Powers)
+            {
+                updateEntries.Add(MakeEntry(() => sensor.Value, metric.Create(MakeTags(new("key", sensor.Key), new("description", sensor.Description)))));
+            }
+        }
+
+        if (smc.Fans.Count > 0)
+        {
+            var metricActual = manager.CreateGauge("hardware_monitor_fan_rpm");
+            foreach (var fan in smc.Fans)
+            {
+                updateEntries.Add(MakeEntry(() => fan.ActualRpm, metricActual.Create(MakeTags([new("index", fan.Index)]))));
+            }
+        }
     }
 }

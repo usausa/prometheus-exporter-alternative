44dd12a96d7ac2229467038bb41c3da617acd63b
usausa <machi.pon@gmail.com>
2026-09-01 10:56:07 +0900

    Update

--- PrometheusExporter.Abstractions/IInstrumentationRegistry.cs
+++ PrometheusExporter.Abstractions/IInstrumentationRegistry.cs
@@ -1,8 +1,6 @@
 namespace PrometheusExporter.Abstractions;
 
-#pragma warning disable CA1711
 public interface IInstrumentationRegistry
 {
     void Register(string name, Action<IInstrumentationBuilder> configure);
 }
-#pragma warning restore CA1711
